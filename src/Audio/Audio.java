360636514108da71e0228f269bd3e161162b5ea3
FabioMarras <fabio.marras@yahoo.com>
2023-10-06 11:35:09 +0200

    add alzaVolume e AbbassaVolume

--- src/Audio/Audio.java
+++ src/Audio/Audio.java
@@ -4,23 +4,71 @@ public class Audio {
     public void sceltaBrano(String scelta) {
         switch (scelta) {
             case "BRANO1", "Brano1", "brano1":
-                System.out.println("Hai scelto BRANO1");
+                System.out.println("Hai scelto: " + TrackList.BRANO1.getNome() + " durata: " +  TrackList.BRANO1.getDurata() + " il volume è: " + TrackList.BRANO1.getVolume());
+               /* alzaVolume(TrackList.BRANO1);
+                System.out.println("Hai scelto: " + TrackList.BRANO1.getNome() + " durata: " +  TrackList.BRANO1.getDurata() + " il volume è: " + TrackList.BRANO1.getVolume());
+                alzaVolume(TrackList.BRANO1);
+                System.out.println("Hai scelto: " + TrackList.BRANO1.getNome() + " durata: " +  TrackList.BRANO1.getDurata() + " il volume è: " + TrackList.BRANO1.getVolume());
+                alzaVolume(TrackList.BRANO1);
+                System.out.println("Hai scelto: " + TrackList.BRANO1.getNome() + " durata: " +  TrackList.BRANO1.getDurata() + " il volume è: " + TrackList.BRANO1.getVolume());
+                abbassaVolume(TrackList.BRANO1);
+                System.out.println("Hai scelto: " + TrackList.BRANO1.getNome() + " durata: " +  TrackList.BRANO1.getDurata() + " il volume è: " + TrackList.BRANO1.getVolume());
+                abbassaVolume(TrackList.BRANO1);
+                System.out.println("Hai scelto: " + TrackList.BRANO1.getNome() + " durata: " +  TrackList.BRANO1.getDurata() + " il volume è: " + TrackList.BRANO1.getVolume());*/
                 break;
-            case "BRANO2":
-                System.out.println("Hai scelto BRANO2");
+            case "BRANO2", "Brano2", "brano2":
+                System.out.println("Hai scelto: " + TrackList.BRANO2.getNome() + " durata: " +  TrackList.BRANO2.getDurata() + " il volume è: " + TrackList.BRANO2.getVolume());
                 break;
-            case "BRANO3":
-                System.out.println("Hai scelto BRANO3");
+            case "BRANO3", "Brano3", "brano3":
+                System.out.println("Hai scelto: " + TrackList.BRANO3.getNome() + " durata: " +  TrackList.BRANO3.getDurata() + " il volume è: " + TrackList.BRANO3.getVolume());
                 break;
-            case "BRANO4":
-                System.out.println("Hai scelto BRANO4");
+            case "BRANO4", "Brano4", "brano4":
+                System.out.println("Hai scelto: " + TrackList.BRANO4.getNome() + " durata: " +  TrackList.BRANO4.getDurata() + " il volume è: " + TrackList.BRANO4.getVolume());
                 break;
-            case "BRANO5":
-                System.out.println("Hai scelto BRANO5");
+            case "BRANO5", "Brano5", "brano5":
+                System.out.println("Hai scelto: " + TrackList.BRANO5.getNome() + " durata: " +  TrackList.BRANO5.getDurata() + " il volume è: " + TrackList.BRANO5.getVolume());
                 break;
             default:
                 System.out.println("Non abbiamo questo brano :(");
                 break;
         }
     }
+//public void StampaBrano() {
+//  if (TrackList.BRANO1.getVolume().length() > 0 && < 2) {
+//      System.out.println(TrackList.BRANO1.getNome());
+//  } if else {
+//      (TrackList.BRANO1.getVolume().length() > 1 && < 3) {
+//            System.out.println(TrackList.BRANO1.getNome() * 2);
+//    }
+//}
+    public void alzaVolume(TrackList brano) {
+        String volumeAttuale = brano.getVolume();
+
+        if (volumeAttuale.length() == 1) {
+            TrackList.BRANO1.setVolume("!!");
+        } else if (volumeAttuale.length() == 2) {
+                TrackList.BRANO1.setVolume("!!!");;
+            } else if (volumeAttuale.length() == 3) {
+            TrackList.BRANO1.setVolume("!!!!");;
+        } else if (volumeAttuale.length() == 4) {
+            TrackList.BRANO1.setVolume("!!!!!");;
+        } else {
+            System.out.println("Volume già al massimo!");
+        }
+        }
+    public void abbassaVolume(TrackList brano) {
+        String volumeAttuale = brano.getVolume();
+        if (volumeAttuale.length() == 5) {
+            TrackList.BRANO1.setVolume("!!!!");
+        } else if (volumeAttuale.length() == 4) {
+            TrackList.BRANO1.setVolume("!!!");;
+        } else if (volumeAttuale.length() == 3) {
+            TrackList.BRANO1.setVolume("!!");;
+        } else if (volumeAttuale.length() == 2) {
+            TrackList.BRANO1.setVolume("!");;
+        } else {
+            System.out.println("Volume già al minimo!");
+        }
+    }
+    
 }
--- src/Audio/TrackList.java
+++ src/Audio/TrackList.java
@@ -1,19 +1,21 @@
 package Audio;
 
 public enum TrackList {
-    BRANO1("Brano 1", 180),
-    BRANO2("Brano 2", 180),
-    BRANO3("Brano 3", 180),
-    BRANO4("Brano 4", 180),
-    BRANO5("Brano 5", 180),
+    BRANO1("Brano 1", 180, "!!!"),
+    BRANO2("Brano 2", 180, "!!"),
+    BRANO3("Brano 3", 180, "!!"),
+    BRANO4("Brano 4", 180, "!!"),
+    BRANO5("Brano 5", 180, "!!"),
     ;
 
     private String nome;
     private int durata;
+    private String volume;
 
-    private TrackList( String nome, int durata){
+    TrackList(String nome, int durata, String volume){
         this.nome = nome;
         this.durata = durata;
+        this.volume = volume;
     }
 
     public String getNome() {
@@ -22,5 +24,13 @@ public enum TrackList {
 
     public int getDurata() {
         return durata;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
     }
 }
